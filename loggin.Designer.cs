abd8569b1f13c32b1873816ae5a758714ce6bcd6
NImnaka00 <152169083+Nimnaka00@users.noreply.github.com>
2025-02-05 17:22:18 +0530

    Create loggin.Designer.cs
    Create loggin.designer.cs

---
+++ loggin.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Skill_IS_System
+{
+    partial class loggin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            login = new Button();
+            panel1 = new Panel();
+            label6 = new Label();
+            username = new RichTextBox();
+            password = new RichTextBox();
+            panel2 = new Panel();
+            label7 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(71, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 51);
+            label1.TabIndex = 0;
+            label1.Text = "SKILL";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(71, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(323, 45);
+            label2.TabIndex = 1;
+            label2.Text = "International School";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(188, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(129, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Login here!";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(104, 177);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Username";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(107, 220);
+            label5.Name = "label5";
+            label5.Size = new Size(82, 21);
+            label5.TabIndex = 4;
+            label5.Text = "password";
+            // 
+            // login
+            // 
+            login.BackColor = SystemColors.MenuHighlight;
+            login.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            login.ForeColor = SystemColors.Control;
+            login.Location = new Point(210, 267);
+            login.Margin = new Padding(3, 2, 3, 2);
+            login.Name = "login";
+            login.Size = new Size(92, 30);
+            login.TabIndex = 5;
+            login.Text = "Login";
+            login.UseVisualStyleBackColor = false;
+            login.Click += login_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Maroon;
+            panel1.Controls.Add(label6);
+            panel1.Location = new Point(452, 9);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(38, 34);
+            panel1.TabIndex = 6;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(3, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(30, 32);
+            label6.TabIndex = 10;
+            label6.Text = "X";
+            label6.Click += label6_Click;
+            // 
+            // username
+            // 
+            username.Location = new Point(210, 182);
+            username.Margin = new Padding(3, 2, 3, 2);
+            username.Name = "username";
+            username.Size = new Size(176, 23);
+            username.TabIndex = 7;
+            username.Text = "";
+            // 
+            // password
+            // 
+            password.Location = new Point(210, 224);
+            password.Margin = new Padding(3, 2, 3, 2);
+            password.Name = "password";
+            password.Size = new Size(176, 23);
+            password.TabIndex = 8;
+            password.Text = "";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.Control;
+            panel2.Controls.Add(label7);
+            panel2.ForeColor = SystemColors.Control;
+            panel2.Location = new Point(2, 320);
+            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(504, 48);
+            panel2.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.ActiveCaptionText;
+            label7.Location = new Point(181, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(134, 17);
+            label7.TabIndex = 0;
+            label7.Text = "Copyright @ SKILL IS";
+            // 
+            // loggin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(501, 364);
+            Controls.Add(panel2);
+            Controls.Add(password);
+            Controls.Add(username);
+            Controls.Add(panel1);
+            Controls.Add(login);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "loggin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button login;
+        private Panel panel1;
+        private RichTextBox username;
+        private RichTextBox password;
+        private Panel panel2;
+        private Label label6;
+        private Label label7;
+    }
+}
